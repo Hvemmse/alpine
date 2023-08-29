8fbbfdad9d9895952d59eefa443788586c6578e8
Frank Simens <84502105+Hvemmse@users.noreply.github.com>
2023-08-29 09:30:04 +0200

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -7,6 +7,8 @@ apk update
 # Installer Xorg
 apk add xorg-server xf86-video-vesa xf86-video-qxl  || exit
 
+setup-xorg-base
+
 
 # Installer Xfce4 og tilknyttede pakker
 apk add xrdp xfce4 xfce4-terminal xfce4-session thunar-volman ttf-dejavu lightdm || exit
