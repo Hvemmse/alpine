91702e718c3b30a02f78fce230e5ebcb3118c002
Frank Simens <84502105+Hvemmse@users.noreply.github.com>
2023-08-29 09:39:27 +0200

    Create user.sh

---
+++ user.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Opret brugeren "live"
+adduser live <<EOF
+live
+live
+Live User
+""
+""
+""
+EOF
+
+# Giv brugeren sudo-adgang
+echo "live ALL=(ALL) ALL" >> /etc/sudoers
+
+# Vis oprettelsesbekræftelse
+echo "Brugeren 'live' er oprettet med adgangskoden 'live' og har sudo-adgang."
