6c13a5fe966c195b5fde84d6d02035c82e19cc02
Frank Simens <84502105+Hvemmse@users.noreply.github.com>
2023-08-29 08:57:19 +0200

    Create xfce4.sh

---
+++ xfce4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Laver en upgradering"
+apk update && apk upgrade || exit
+
+
+# Vis en besked og vent på et tastetryk
+read -n 1 -s -r -p "Tryk på en tast for at fortsætte..."
+
+# Din kode fortsætter her efter tastetrykket
+echo "Fortsætter nu..."
+
+echo "Installerer micro tmux ranger openssh samba"
+apk add xfce4 xorg xrdp openssh samba || exit
