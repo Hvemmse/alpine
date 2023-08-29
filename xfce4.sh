9e0a0667b7b95079b9c7479667f6aa5538edc1cc
Frank Simens <84502105+Hvemmse@users.noreply.github.com>
2023-08-29 09:07:43 +0200

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -7,10 +7,14 @@ apk update
 apk add xorg-server xf86-video-vesa || exit
 
 # Konfigurer Xorg til at starte automatisk under opstart
-rc-update add xdm default || exit
+rc-update add lightdm default || exit
 
 # Installer Xfce4 og tilknyttede pakker
-apk add xfce4 xfce4-terminal xfce4-session thunar-volman ttf-dejavu || exit
+apk add xrdp xfce4 xfce4-terminal xfce4-session thunar-volman ttf-dejavu || exit
+
+# Konfigurer xRdp til at starte automatisk under opstart
+rc-update add xrdp default || exit
+
 
 # Genstart maskinen
 # reboot
