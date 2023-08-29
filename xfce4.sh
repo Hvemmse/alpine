6410cbe20f208a91e165f5f1780520a9f8d63dc6
Frank Simens <84502105+Hvemmse@users.noreply.github.com>
2023-08-29 09:14:52 +0200

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -1,3 +1,4 @@
+  GNU nano 6.3                                xfce4.sh                                           
 #!/bin/bash
 
 # Opdater pakkeindeks
@@ -6,11 +7,13 @@ apk update
 # Installer Xorg
 apk add xorg-server xf86-video-vesa || exit
 
+
+# Installer Xfce4 og tilknyttede pakker
+apk add xrdp xfce4 xfce4-terminal xfce4-session thunar-volman ttf-dejavu lightdm || exit
+
 # Konfigurer Xorg til at starte automatisk under opstart
 rc-update add lightdm default || exit
 
-# Installer Xfce4 og tilknyttede pakker
-apk add xrdp xfce4 xfce4-terminal xfce4-session thunar-volman ttf-dejavu || exit
 
 # Konfigurer xRdp til at starte automatisk under opstart
 rc-update add xrdp default || exit
